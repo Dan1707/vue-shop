84049227be3f7c21f5ed96f4c01641ea326da2a7
Dan1707 <danuhae@gmail.com>
2024-01-14 10:17:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({});
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+<style lang="scss" scoped></style>
